88eb0f4219594fa5fdd2db5f9f0b14373c349d77
TuranTalayhan <tura0015@gmail.com>
2024-06-11 15:19:51 +0200

    created getters and setters

--- Client/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Game.java
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Game.java
@@ -13,4 +13,44 @@ public class Game {
     private int turnID;
     @Expose
     private int numPlayers;
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public int getBoardID() {
+        return boardID;
+    }
+
+    public void setBoardID(int boardID) {
+        this.boardID = boardID;
+    }
+
+    public int getGameStatus() {
+        return gameStatus;
+    }
+
+    public void setGameStatus(int gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+    public int getTurnID() {
+        return turnID;
+    }
+
+    public void setTurnID(int turnID) {
+        this.turnID = turnID;
+    }
+
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+
+    public void setNumPlayers(int numPlayers) {
+        this.numPlayers = numPlayers;
+    }
 }
