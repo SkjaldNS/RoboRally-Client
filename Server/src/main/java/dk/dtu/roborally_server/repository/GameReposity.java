b6aa5cf67d492ea66153038241cd5ccc64beae59
Nikolaj Schæbel s220471 <s220471@dtu.dk>
2024-06-11 10:41:22 +0200

    Fixed out dated main

--- Server/src/main/java/dk/dtu/roborally_server/repository/GameReposity.java
+++
@@ -1,7 +0,0 @@
-package dk.dtu.roborally_server.repository;
-
-import dk.dtu.roborally_server.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameReposity extends JpaRepository<Game, Long> {
-}
