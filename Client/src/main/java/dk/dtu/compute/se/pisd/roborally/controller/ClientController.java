e3c1eb522f427a86066fb844c6cfbd0a6b04cf80
TuranTalayhan <tura0015@gmail.com>
2024-06-11 15:20:35 +0200

    Created putGameID, putGameBoardID, putGameStatus methods

--- Client/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ClientController.java
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ClientController.java
@@ -3,6 +3,8 @@ package dk.dtu.compute.se.pisd.roborally.controller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dk.dtu.compute.se.pisd.roborally.model.Command;
+import dk.dtu.compute.se.pisd.roborally.model.Game;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -17,10 +19,42 @@ public class ClientController {
         httpClient = HttpClient.newHttpClient();
     }
 
+    // TODO Should gameID be generated in client or server?
     public void postGame() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(BASE_URL + "/games"))
                 .POST(HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    public void putGameID(String oldGameID, String newGameID) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/games/" + oldGameID))
+                .PUT(HttpRequest.BodyPublishers.ofString(newGameID))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    public void putGameBoardID(String gameID, String boardID) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/games/" + gameID))
+                .PUT(HttpRequest.BodyPublishers.ofString(boardID))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+
+    public void putGameStatus(String gameID, String gameStatus) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/games/" + gameID))
+                .PUT(HttpRequest.BodyPublishers.ofString(gameStatus))
                 .build();
 
         HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
