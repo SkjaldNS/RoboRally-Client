f7e5ff7f57957e028cc28ffcd0fed5dfe08f45b7
TuranTalayhan <tura0015@gmail.com>
2024-06-11 15:58:12 +0200

    created PUT,POST,GET methods for player, games and moves

--- Client/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ClientController.java
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dk.dtu.compute.se.pisd.roborally.model.Command;
 import dk.dtu.compute.se.pisd.roborally.model.Game;
+import dk.dtu.compute.se.pisd.roborally.model.Move;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,13 +23,49 @@ public class ClientController {
         httpClient = HttpClient.newHttpClient();
     }
 
-    // TODO Should gameID be generated in client or server?
+    //TODO change return type and return something
+    public void postPlayer() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/players"))
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        //Returns playerID
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    public void putPlayer(Player player) throws Exception {
+        String playerJson = gson.toJson(player);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/players/" + player.getPlayerID()))
+                .PUT(HttpRequest.BodyPublishers.ofString(playerJson))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    //TODO change return type and return something
+    public void getPlayers() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/players"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+
+    //TODO change return type and return something
     public void postGame() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(BASE_URL + "/games"))
                 .POST(HttpRequest.BodyPublishers.noBody())
                 .build();
 
+        //Returns gameID
         HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
@@ -43,12 +81,47 @@ public class ClientController {
         System.out.println(response.body());
     }
 
+    //TODO change return type and return something
+    public void getGames() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/games"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+
     public void postMove(Command command) throws Exception {
         String commandJson = gson.toJson(command);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(BASE_URL + "/moves"))
                 .POST(HttpRequest.BodyPublishers.ofString(commandJson))
                 .header("Content-Type", "application/json")
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    //TODO How is a move identified? Check if this is correct
+    public void putMove(Move move) throws Exception {
+        String gameJson = gson.toJson(move);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/moves/" + move.getGameID() + move.getPlayerID() + move.getTurnID()))
+                .PUT(HttpRequest.BodyPublishers.ofString(gameJson))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    //TODO change return type and return something
+    public void getMoves() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(BASE_URL + "/moves"))
+                .GET()
                 .build();
 
         HttpResponse<String> response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
