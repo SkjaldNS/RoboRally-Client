129b9a46113aa53c7a97bec4050b9da36fc3199d
TuranTalayhan <tura0015@gmail.com>
2024-06-11 15:55:26 +0200

    Created getters and setters for fields

--- Client/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Move.java
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Move.java
@@ -19,4 +19,68 @@ public class Move {
     private int playerID;
     @Expose
     private int turnID;
+
+    public Command getReg1() {
+        return reg1;
+    }
+
+    public void setReg1(Command reg1) {
+        this.reg1 = reg1;
+    }
+
+    public Command getReg2() {
+        return reg2;
+    }
+
+    public void setReg2(Command reg2) {
+        this.reg2 = reg2;
+    }
+
+    public Command getReg3() {
+        return reg3;
+    }
+
+    public void setReg3(Command reg3) {
+        this.reg3 = reg3;
+    }
+
+    public Command getReg4() {
+        return reg4;
+    }
+
+    public void setReg4(Command reg4) {
+        this.reg4 = reg4;
+    }
+
+    public Command getReg5() {
+        return reg5;
+    }
+
+    public void setReg5(Command reg5) {
+        this.reg5 = reg5;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getTurnID() {
+        return turnID;
+    }
+
+    public void setTurnID(int turnID) {
+        this.turnID = turnID;
+    }
 }
