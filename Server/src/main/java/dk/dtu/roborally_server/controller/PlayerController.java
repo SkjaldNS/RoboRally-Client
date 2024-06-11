883f2bcea31240f3acb991987e41aa33cda7c671
Marcus Langkilde <s195080@dtu.dk>
2024-06-11 20:58:23 +0200

    added postmapping and its functionality

--- Server/src/main/java/dk/dtu/roborally_server/controller/PlayerController.java
+++ Server/src/main/java/dk/dtu/roborally_server/controller/PlayerController.java
@@ -2,10 +2,9 @@ package dk.dtu.roborally_server.controller;
 
 import dk.dtu.roborally_server.model.Player;
 import dk.dtu.roborally_server.repository.PlayerRepository;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,27 @@ public class PlayerController {
     public ResponseEntity<List<Player>> getPlayers(){
         List<Player> playerList = playerRepository.findAll();
         return ResponseEntity.ok(playerList);
+    }
+    /*
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "")
+    public ResponseEntity<String> createPlayer(@RequestBody Player player) {
+        if(player.getPlayerName() == null || player.getGameId() == null)
+            return ResponseEntity.badRequest().body("Name and GameId must be provided");
+        if(playerRepository.findPlayerByNameAndGameId(player.getPlayerName(), player.getGameId()) != null)
+            return ResponseEntity.badRequest().body("Player already exists");
+        playerRepository.save(player);
+        return ResponseEntity.ok().build();
+    }
+    */
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "")
+    public ResponseEntity<String> createPlayer(@RequestBody Player player) {
+        if(player.getPlayerName() == null)
+            return ResponseEntity.badRequest().body("Name and GameId must be provided");
+        if(playerRepository.findPlayerByName(player.getPlayerName())!= null)
+            return ResponseEntity.badRequest().body("Player already exists");
+        playerRepository.save(player);
+        return ResponseEntity.ok().build();
     }
 }
