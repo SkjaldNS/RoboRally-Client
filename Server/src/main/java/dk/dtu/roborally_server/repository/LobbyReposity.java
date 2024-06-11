75ab985d7f45c0abb6c2cdfa80d90be44aad150a
Nikolaj Schæbel s220471 <s220471@dtu.dk>
2024-06-11 12:35:07 +0200

    Removed lobby related files

--- Server/src/main/java/dk/dtu/roborally_server/repository/LobbyReposity.java
+++
@@ -1,7 +0,0 @@
-package dk.dtu.roborally_server.repository;
-
-import dk.dtu.roborally_server.model.Lobby;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LobbyReposity extends JpaRepository<Lobby, Long> {
-}
