b56d66c5823824f218125dfad786788e72fd24a7
Marcus Langkilde <s195080@dtu.dk>
2024-06-12 09:35:36 +0200

    spelling error

--- Server/src/main/java/dk/dtu/roborally_server/controller/GameController.java
+++ Server/src/main/java/dk/dtu/roborally_server/controller/GameController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/game")
+@RequestMapping("/games")
 public class GameController {
 
     private GameRepository gameRepository;
