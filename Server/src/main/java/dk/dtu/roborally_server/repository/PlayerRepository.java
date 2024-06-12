1bc6b7d2319897980c62e6c98f11e242a2f41614
Marcus Langkilde <s195080@dtu.dk>
2024-06-12 23:29:57 +0200

    added the method findPlayerById

--- Server/src/main/java/dk/dtu/roborally_server/repository/PlayerRepository.java
+++ Server/src/main/java/dk/dtu/roborally_server/repository/PlayerRepository.java
@@ -3,9 +3,13 @@ package dk.dtu.roborally_server.repository;
 import dk.dtu.roborally_server.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
 
     public Player findPlayerByPlayerName(String playerName);
+
+    public Optional<Player> findPlayerById(Long playerId);
 }
