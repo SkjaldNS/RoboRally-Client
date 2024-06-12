ed21e5534ab0f0bcacaca03614393dfa3357cfe9
Nikolaj Schæbel / s220471 <s220471@dtu.dk>
2024-06-12 11:26:43 +0200

    Split Player into Player and PlayerLocal, where PlayerLocal is a child class of player with additional logic (discardpile, deck, cards)

---
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/model/PlayerLocal.java
@@ -0,0 +1,42 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import com.google.gson.annotations.Expose;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * PlayerLocal extends the Player class and implements the nescessary logic for a local player.
+ *
+ * @Author Nikolaj Schæbel, s220471
+ *
+ * */
+public class PlayerLocal extends Player {
+
+    private DiscardPileField discardedPile;
+    private Deck deck;
+    private CommandCardField[] cards;
+    final public static int NO_CARDS = 8;
+
+    public PlayerLocal(@NotNull Board board, int robotId, @NotNull String name) {
+        super(board, robotId, name);
+
+        cards = new CommandCardField[NO_CARDS];
+        for (int i = 0; i < cards.length; i++) {
+            cards[i] = new CommandCardField(this);
+        }
+
+        discardedPile = new DiscardPileField(this);
+        deck = new Deck();
+        deck.shuffleDeck();
+    }
+
+    public Deck getDeck() {return deck;}
+
+    public DiscardPileField getDiscardedPile() {return discardedPile;}
+
+    public CommandCardField getCardField(int i) {return cards[i];}
+
+    @Override
+    public boolean isLocalPlayer() {
+        return true;
+    }
+}
