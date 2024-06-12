91421f55ca86f809b42ba04de0d856ebf35bd517
Marcus Langkilde <s195080@dtu.dk>
2024-06-12 21:57:32 +0200

    fixed a spelling mistake in the method for finding player

--- Server/src/main/java/dk/dtu/roborally_server/repository/PlayerRepository.java
+++ Server/src/main/java/dk/dtu/roborally_server/repository/PlayerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
 
-    public Player findPlayerByName(String playerName);
+    public Player findPlayerByPlayerName(String playerName);
 }
