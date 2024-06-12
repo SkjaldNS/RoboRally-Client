ff99dee8ff8b248feb1a43146a857be9860bfd7b
Asma <s230716@dtu.dk>
2024-06-12 13:07:58 +0200

    "Map button working and we can choose a map."

--- Client/src/main/java/dk/dtu/compute/se/pisd/roborally/view/AdminLobbyMap.java
+++ Client/src/main/java/dk/dtu/compute/se/pisd/roborally/view/AdminLobbyMap.java
@@ -2,10 +2,9 @@ package dk.dtu.compute.se.pisd.roborally.view;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -16,20 +15,50 @@ import javafx.scene.shape.Rectangle;
  * @author Asma Maryam, s230716
  */
 public class AdminLobbyMap extends VBox {
+
+    private ImageView mapPreview;
+    private Rectangle mapPreviewBackground;
+
     public AdminLobbyMap() {
         ComboBox<String> mapSelection = new ComboBox<>();
         mapSelection.getItems().addAll("Map 1");
         mapSelection.setPromptText("Select Map");
 
         // Placeholder for map preview
-        ImageView mapPreview = new ImageView();
+        mapPreview = new ImageView();
         mapPreview.setFitWidth(200);
         mapPreview.setFitHeight(200);
         mapPreview.setStyle("-fx-border-color: black; -fx-border-width: 10px;");
 
-        Rectangle mapPreviewBackground = new Rectangle(200, 200);
+        mapPreviewBackground = new Rectangle(200, 200);
         mapPreviewBackground.setFill(Color.LIGHTGRAY);
-        getChildren().addAll(mapSelection, mapPreviewBackground);
+
+        getChildren().addAll(mapSelection, mapPreviewBackground, mapPreview);
         this.alignmentProperty().set(Pos.CENTER_RIGHT);
+
+        // Add event handler for ComboBox selection changes
+        mapSelection.setOnAction(event -> {
+            String selectedMap = mapSelection.getValue();
+            if ("Map 1".equals(selectedMap)) {
+                updateMapPreview("map_image/map_1.png");
+            }
+        });
+    }
+
+    private void updateMapPreview(String imagePath) {
+        try {
+            // Correctly load the image from the resources folder
+            Image mapImage = new Image(getClass().getClassLoader().getResourceAsStream(imagePath));
+            if (mapImage == null) {
+                throw new IllegalArgumentException("Image not found: " + imagePath);
+            }
+
+            // Remove the placeholder and set the image
+            getChildren().remove(mapPreviewBackground);
+            mapPreview.setImage(mapImage);
+        } catch (Exception e) {
+            System.err.println("Could not load image: " + imagePath);
+            e.printStackTrace();
+        }
     }
 }
