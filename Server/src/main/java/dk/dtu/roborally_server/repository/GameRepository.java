711c6a5d00ce48f12952b8382e66763e8fcf9346
Marcus Langkilde <s195080@dtu.dk>
2024-06-13 12:36:27 +0200

    added findgamebygameid

--- Server/src/main/java/dk/dtu/roborally_server/repository/GameRepository.java
+++ Server/src/main/java/dk/dtu/roborally_server/repository/GameRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     Game findGameByGameName(String gameName);
+    int findNumberOfPlayersByGameId(Long gameId);
+
+    Game findGameByGameId(Long gameId);
 }
